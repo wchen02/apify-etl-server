46361334281f7f073c9641615f76f758c04cf92e
Wensheng Chen <icydragoon@gmail.com>
2019-08-18 05:07:48 -0700

    fix: Run nodejs server on sub uri

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const apify = express();
 const port = 3000;
 
+app.get('/', (req, res) => res.send('HEY THERE!'));
 app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/process-dataset', (req, res) => {
+apify.get('/process-dataset', (req, res) => {
     res.send('Hello from process-dataset!');
 });
-
+app.use('/apify', apify);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
